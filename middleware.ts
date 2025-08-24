1394477d1e5fa62fa6e05fb8329f1c2cd6c4ca7a
Obori Tateki(大堀 建城) <tateki.obori@cct-inc.co.jp>
2025-08-24 21:05:48 +0900

    Update: Chapter 16まで完了。App Router finish

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+
+export { auth as middleware } from "./auth.config";
+ 
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  runtime: 'nodejs',
+};
